93e5e5725cf1eef8f256e61580c6849bda5b360f
minimonk <morpheuz@hanmail.net>
2020-06-07 11:57:04 +0900

    change text when button click for debug

--- app/src/main/java/com/example/rpiremotecontrol/MainActivity.java
+++ app/src/main/java/com/example/rpiremotecontrol/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rpiremotecontrol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onFRONT(android.view.View view)
+    {
+        final TextView txt1  = (TextView)findViewById(R.id.ID_TEXT_STATUS);
+        txt1.setText("FRONT");
+    }
+
+    public void onSTOP(android.view.View view)
+    {
+        final TextView txt1  = (TextView)findViewById(R.id.ID_TEXT_STATUS);
+        txt1.setText("STOP");
+    }
+
+    public void onREAR(android.view.View view)
+    {
+        final TextView txt1  = (TextView)findViewById(R.id.ID_TEXT_STATUS);
+        txt1.setText("REAR");
+    }
+
+    public void onLEFT(android.view.View view)
+    {
+        final TextView txt1  = (TextView)findViewById(R.id.ID_TEXT_STATUS);
+        txt1.setText("LEFT");
+    }
+
+    public void onCENTER(android.view.View view)
+    {
+        final TextView txt1  = (TextView)findViewById(R.id.ID_TEXT_STATUS);
+        txt1.setText("CENTER");
+    }
+
+    public void onRIGHT(android.view.View view)
+    {
+        final TextView txt1  = (TextView)findViewById(R.id.ID_TEXT_STATUS);
+        txt1.setText("RIGHT");
+    }
+
 }
